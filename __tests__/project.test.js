98bc1f84a068d515523f741e2e722b4226519c5f
Joshua Forman <cyborgforman@gmail.com>
2019-11-15 10:14:43 -0800

    add a test for mercury age

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -6,4 +6,8 @@ describe('GalacticAge', () => {
     console.log(age);
     expect(age.earthAge).toEqual(5);
   });
+  test('should correctly return Mercury age', () => {
+    let age = new GalacticAge(100);
+    expect(age.mercuryAge).toEqual(5);
+  });
 });
