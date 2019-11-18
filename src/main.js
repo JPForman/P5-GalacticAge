c4b12cd41e4a0e661d77f43d65fa77615f8c50f7
Joshua Forman <cyborgforman@gmail.com>
2019-11-17 22:30:31 -0800

    add html and styles for functional user interface

--- src/main.js
+++ src/main.js
@@ -8,10 +8,16 @@ import { GalacticAge } from './../src/galacticAge.js';
 $(document).ready(function() {
   $("form#ageForm").submit(function(event) {
     event.preventDefault();
+    $("#ageForm").hide();
+    $(".ageReveal").show();
     let ageInput = $("#ageInput").val();
     let userAges = new GalacticAge(ageInput);
     console.log(userAges);
 
-
+    $("#earthOut").text(userAges.earthAge)
+    $("#mercuryOut").text(userAges.mercuryAge)
+    $("#venusOut").text(userAges.venusAge)
+    $("#marsOut").text(userAges.marsAge)
+    $("#jupiterOut").text(userAges.jupiterAge)
   });
 });
