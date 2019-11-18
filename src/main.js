8827e2cb81e7885ef52624b17a585ecdec0b32f8
Joshua Forman <cyborgforman@gmail.com>
2019-11-17 22:51:08 -0800

    edit front end verbage

--- src/main.js
+++ src/main.js
@@ -14,10 +14,15 @@ $(document).ready(function() {
     let userAges = new GalacticAge(ageInput);
     console.log(userAges);
 
-    $("#earthOut").text(userAges.earthAge)
-    $("#mercuryOut").text(userAges.mercuryAge)
-    $("#venusOut").text(userAges.venusAge)
-    $("#marsOut").text(userAges.marsAge)
-    $("#jupiterOut").text(userAges.jupiterAge)
+    $("#earthOut").text(Math.round( userAges.earthAge * 10) / 10);
+    $("#mercuryOut").text(Math.round( userAges.mercuryAge * 10) / 10);
+    $("#venusOut").text(Math.round( userAges.venusAge * 10) / 10);
+    $("#marsOut").text(Math.round( userAges.marsAge * 10) / 10);
+    $("#jupiterOut").text(Math.round( userAges.jupiterAge * 10) / 10);
+    // $("#earthOut").text(userAges.earthAge)
+    // $("#mercuryOut").text(userAges.mercuryAge)
+    // $("#venusOut").text(userAges.venusAge)
+    // $("#marsOut").text(userAges.marsAge)
+    // $("#jupiterOut").text(userAges.jupiterAge)
   });
 });
