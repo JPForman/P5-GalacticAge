4f3703a5a40683fd78dfe199274ab71f5a1c864a
Joshua Forman <cyborgforman@gmail.com>
2019-11-23 10:33:50 -0800

    add accuracy to backend logic and tests

--- __tests__/project.test.js
+++ __tests__/project.test.js
@@ -6,24 +6,24 @@ describe('GalacticAge', () => {
     expect(age.earthAge).toEqual(5);
   });
   test('should correctly return Mercury age', () => {
-    let age = new GalacticAge(100);
-    expect(age.mercuryAge).toEqual(24);
+    let age = new GalacticAge(24);
+    expect(age.mercuryAge).toEqual(100);
   });
   test('should correctly return Venus age', () => {
-    let age = new GalacticAge(100);
-    expect(age.venusAge).toEqual(62);
+    let age = new GalacticAge(62);
+    expect(age.venusAge).toEqual(100);
   });
   test('should correctly return Mars age', () => {
-    let age = new GalacticAge(100);
-    expect(age.marsAge).toEqual(188);
+    let age = new GalacticAge(188);
+    expect(age.marsAge).toEqual(100);
   });
   test('should correctly return Jupiter age', () => {
-    let age = new GalacticAge(100);
-    expect(age.jupiterAge).toEqual(1186);
+    let age = new GalacticAge(1186);
+    expect(age.jupiterAge).toEqual(100);
   });
   test('should correctly return time left on Earth when positive', () => {
-    let age = new GalacticAge(50);
-    expect(age.earthLeft()).toEqual(30);
+    let age = new GalacticAge(90);
+    expect(age.earthLeft()).toEqual('You outlived expectations by 10 years');
   });
   test('should correctly return time left on Earth when negative', () => {
     let age = new GalacticAge(82);
@@ -31,34 +31,34 @@ describe('GalacticAge', () => {
   });
   test('should correctly return time left on Mercury when positive', () => {
     let age = new GalacticAge(50);
-    expect(age.mercuryLeft()).toEqual(7);
+    expect(age.mercuryLeft()).toEqual(125);
   });
   test('should correctly return time left on Mercury when negative', () => {
     let age = new GalacticAge(100);
-    expect(age.mercuryLeft()).toEqual(`You outlived expectations by about 5 years`);
+    expect(age.mercuryLeft()).toEqual(`You outlived expectations by about 83 years`);
   });
   test('should correctly return time left on Venus when positive', () => {
     let age = new GalacticAge(50);
-    expect(age.venusLeft()).toEqual(19);
+    expect(age.venusLeft()).toEqual(48);
   });
   test('should correctly return time left on Venus when negative', () => {
     let age = new GalacticAge(100);
-    expect(age.venusLeft()).toEqual("You outlived expectations by about 12 years");
+    expect(age.venusLeft()).toEqual("You outlived expectations by about 32 years");
   });
   test('should correctly return time left on Mars when positive', () => {
     let age = new GalacticAge(50);
-    expect(age.marsLeft()).toEqual(56);
+    expect(age.marsLeft()).toEqual(16);
   });
   test('should correctly return time left on Mars when negative', () => {
     let age = new GalacticAge(100);
-    expect(age.marsLeft()).toEqual("You outlived expectations by about 38 years");
+    expect(age.marsLeft()).toEqual("You outlived expectations by about 11 years");
   });
   test('should correctly return time left on Jupiter when positive', () => {
-    let age = new GalacticAge(40);
-    expect(age.jupiterLeft()).toEqual(474);
+    let age = new GalacticAge(50);
+    expect(age.jupiterLeft()).toEqual(3);
   });
   test('should correctly return time left on Jupiter when negative', () => {
     let age = new GalacticAge(100);
-    expect(age.jupiterLeft()).toEqual("You outlived expectations by about 237 years");
+    expect(age.jupiterLeft()).toEqual("You outlived expectations by about 2 years");
   });
 });
--- src/galacticAge.js
+++ src/galacticAge.js
@@ -21,7 +21,7 @@ export class GalacticAge {
   }
 
   mercuryLeft (mercuryAge) {
-    let mercuryLeft = Math.round((.24*80) - this.mercuryAge);
+    let mercuryLeft = Math.round((80/.24) - this.mercuryAge);
     if (mercuryLeft <= 0) {
       let mercuryNeg = (-1 * mercuryLeft);
       return "You outlived expectations by about " + mercuryNeg + " years";
@@ -31,7 +31,10 @@ export class GalacticAge {
   }
 
   venusLeft (venusAge) {
-    let venusLeft = Math.round((.62*80) - this.venusAge);
+    let venusLeft = Math.round((80/.62) - this.venusAge);
+    console.log(80/.62);
+    console.log(this.venusAge);
+    console.log(venusLeft);
     if (venusLeft <= 0) {
       let venusNeg = (-1 * venusLeft);
       return "You outlived expectations by about " + venusNeg + " years";
@@ -40,7 +43,7 @@ export class GalacticAge {
     }
   }
   marsLeft (marsAge) {
-    let marsLeft = Math.round((1.88*80) - this.marsAge);
+    let marsLeft = Math.round((80/1.88) - this.marsAge);
     if (marsLeft <= 0) {
       let marsNeg = (-1 * marsLeft);
       return "You outlived expectations by about " + marsNeg + " years";
@@ -49,7 +52,7 @@ export class GalacticAge {
     }
   }
   jupiterLeft (jupiterAge) {
-    let jupiterLeft = Math.round((11.86*80) - this.jupiterAge);
+    let jupiterLeft = Math.round((80/11.86) - this.jupiterAge);
     if (jupiterLeft <= 0) {
       let jupiterNeg = (-1 * jupiterLeft);
       return "You outlived expectations by about " + jupiterNeg + " years";
